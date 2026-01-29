21d04bb933587f92bd535d2833ab3867ee648a0e
srivankumar <adupasrivankumar@gmail.com>
2026-01-29 16:59:47 +0530

    29-01-04

--- 27-01-2026/Problem5.java
+++ 27-01-2026/Problem5.java
@@ -1,38 +1,17 @@
 import java.io.*;
 import java.util.*;
-import java.util.logging.*;
-
-class InvalidStudentException extends Exception {
-    public InvalidStudentException(String msg) {
-        super(msg);
-    }
-}
 
 class Student {
-    Integer id;
+    int id;
     String name;
-    Integer age;
+    int age;
 
-    Student(Integer id, String name, Integer age) {
+    Student(int id, String name, int age) {
         this.id = id;
         this.name = name;
         this.age = age;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Student)) return false;
-        Student s = (Student) o;
-        return id.equals(s.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return id.hashCode();
-    }
-
-    @Override
     public String toString() {
         return id + "," + name + "," + age;
     }
@@ -40,14 +19,9 @@ class Student {
 
 public class Problem5 {
 
-    private static final Logger logger = Logger.getLogger("StudentLog");
-
     public static void main(String[] args) {
 
-        setupLogger();
-
-        List<Student> validList = new ArrayList<>();
-        Set<Student> uniqueSet = new HashSet<>();
+        HashMap<Integer, Student> studentMap = new HashMap<>();
         List<String> invalidList = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader("students.txt"))) {
@@ -56,70 +30,54 @@ public class Problem5 {
 
             while ((line = br.readLine()) != null) {
                 try {
-                    Student s = parseStudent(line);
-                    if (!uniqueSet.add(s)) {
-                        throw new InvalidStudentException("Duplicate student");
+                    String[] arr = line.split(",");
+
+                    if (arr.length != 3) {
+                        throw new Exception("Wrong format");
                     }
-                    validList.add(s);
+
+                    int id = Integer.parseInt(arr[0].trim());
+                    String name = arr[1].trim().replaceAll("\\s+", " ");
+                    int age = Integer.parseInt(arr[2].trim());
+
+                    if (age < 18) {
+                        throw new Exception("Age below 18");
+                    }
+
+                    if (studentMap.containsKey(id)) {
+                        throw new Exception("Duplicate ID");
+                    }
+
+                    studentMap.put(id, new Student(id, name, age));
+
                 } catch (Exception e) {
                     invalidList.add(line);
-                    logger.warning("Invalid: " + line + " | " + e.getMessage());
                 }
             }
 
-            writeFile("valid.txt", validList);
-            writeFile("invalid.txt", invalidList);
-
-            logger.info("File processing completed");
+            writeValid("valid.txt", studentMap.values());
+            writeInvalid("invalid.txt", invalidList);
 
         } catch (IOException e) {
-            logger.severe("File read error: " + e.getMessage());
+            System.out.println("File error");
         }
     }
 
-    private static Student parseStudent(String line) throws InvalidStudentException {
-
-        String[] arr = line.split(",");
-
-        if (arr.length != 3) {
-            throw new InvalidStudentException("Wrong format");
-        }
-
-        try {
-            Integer id = Integer.valueOf(arr[0].trim());
-            String name = cleanName(arr[1]);
-            Integer age = Integer.valueOf(arr[2].trim());
-
-            if (age < 18) {
-                throw new InvalidStudentException("Age below 18");
-            }
-
-            return new Student(id, name, age);
-
-        } catch (NumberFormatException e) {
-            throw new InvalidStudentException("Invalid number");
-        }
-    }
-
-    private static String cleanName(String name) {
-        return new StringBuilder(name.trim().replaceAll("\\s+", " ")).toString();
-    }
-
-    private static void writeFile(String file, List<?> data) throws IOException {
+    static void writeValid(String file, Collection<Student> list) throws IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-        for (Object o : data) {
-            bw.write(o.toString());
+        for (Student s : list) {
+            bw.write(s.toString());
             bw.newLine();
         }
         bw.close();
     }
 
-    private static void setupLogger() {
-        try {
-            FileHandler fh = new FileHandler("app.log", true);
-            fh.setFormatter(new SimpleFormatter());
-            logger.addHandler(fh);
-            logger.setUseParentHandlers(false);
-        } catch (IOException ignored) {}
+    static void writeInvalid(String file, List<String> list) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        for (String s : list) {
+            bw.write(s);
+            bw.newLine();
+        }
+        bw.close();
     }
 }
